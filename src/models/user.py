3f2b2df66fd462eca8e1ff620ff406e7e4ad7074
Mitunkumarsahu <mitunkumarsahu@gmail.com>
2025-03-01 12:12:03 +0530

    added ist date time column in users table

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy import *
-from datetime import datetime
+from datetime import datetime, timedelta
 from src.db import Base
 
 class User(Base):
@@ -12,6 +12,7 @@ class User(Base):
     website_id = Column(VARCHAR(255), nullable=True)
     website_password = Column(VARCHAR(255), nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow)
+    created_at_ist = Column(DateTime, default=lambda: datetime.utcnow() + timedelta(hours=5, minutes=30))
 
 
 
