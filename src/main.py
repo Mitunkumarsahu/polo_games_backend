cf7f685db096e1df04bc85d36aa32bce92a4393c
Mitunkumarsahu <mitunkumarsahu@gmail.com>
2025-03-18 19:05:41 +0530

    revamp

--- src/main.py
+++ src/main.py
@@ -12,9 +12,7 @@ from src.routers.imagelinkrouter import image_link_router
 from src.routers.imagelinkrouterforbackup import image_link_router_for_backup
 from src.routers.visitors import visitor_router
 from src.routers.offer import offer_router
-from src.routers.matchdata import match_router
 from src.routers.socialmedia import socialmedia_router
-from src.routers.fantasy import fantasy_router
 from src.db import initialize_database
 from src.firstsuperadmin import create_first_superadmin
 from fastapi.middleware.cors import CORSMiddleware
@@ -70,8 +68,6 @@ app.include_router(image_link_router, prefix="/api/imagelink", tags=["ImageLink"
 app.include_router(image_link_router_for_backup, prefix="/api/imagelinkforbackup", tags=["ImageLinkForBackup"])
 app.include_router(visitor_router, prefix="/api/visitors", tags=["Visitors"])
 app.include_router(offer_router, prefix="/api/offers", tags=["Offers"])
-app.include_router(match_router, prefix="/api/match", tags=["Match"])
-app.include_router(fantasy_router, prefix="/api/fantasy", tags=["Fantasy"])
 app.include_router(socialmedia_router, prefix="/api/socialmedia", tags=["SocialMedia"])
 
 @app.on_event("startup")
